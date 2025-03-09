f73d9c632e14c3aaa8597020cd7b057d4dc46f20
munawarvp <vpmunawar007@gmail.com>
2025-03-09 14:44:06 +0530

    setup: stating branch config added

--- llfit/llfit/settings.py
+++ llfit/llfit/settings.py
@@ -98,9 +98,13 @@ WSGI_APPLICATION = 'llfit.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
